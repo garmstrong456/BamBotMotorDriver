85c29826ee75b96f142335fef9667be3edd8d47c
Greg Armstrong <garmstrong456@gmail.com>
2018-06-16 14:02:32 -0400

    encoders now working

--- BamBotMotorDriver.cpp
+++ BamBotMotorDriver.cpp
@@ -2,109 +2,95 @@
 #include "BamBotMotorDriver.h"
 #include <Adafruit_MCP23008.h>
 
-//Define the PWM Channels, frequency and precision
-byte BamBotMotorDriver::_M1CHN = 1;
-byte BamBotMotorDriver::_M2CHN = 2;
-byte BamBotMotorDriver::PWM_PRECISION = 13;
-int BamBotMotorDriver::PWM_FREQUENCY = 5000;
 
-bool BamBotMotorDriver::_USING_MCP;
-Adafruit_MCP23008 BamBotMotorDriver::_MCP;
-
-//Motor pins and direction flags
-byte BamBotMotorDriver::_M1DIR;
-byte BamBotMotorDriver::_M2DIR;
-byte BamBotMotorDriver::_M1PWM;
-byte BamBotMotorDriver::_M2PWM;
-bool BamBotMotorDriver::_flipM1 = false;
-bool BamBotMotorDriver::_flipM2 = false;
-
-//Encoders
-BamBotEncoder BamBotMotorDriver::encoder1;
-BamBotEncoder BamBotMotorDriver::encoder2;
-
-
+/*******************
+Motor Initialization
+********************/
 
 //initialize the motor driver assuming all four pins are connected 
 //directly to the M5Stack
-void BamBotMotorDriver::init(byte M1PWM, 
-										byte M1DIR, 
-										byte M2PWM, 
-										byte M2DIR)
+void BamBotMotorDriver::init(byte M1Pwm, 
+										byte M1Dir, 
+										byte M2Pwm, 
+										byte M2Dir)
 {
 	_USING_MCP = false;
 
 	//Record the motor pins in private variables
-	_M1PWM = M1PWM;
-	_M1DIR = M1DIR;
-	_M2PWM = M2PWM;
-	_M2DIR = M2DIR;
+	_M1Pwm = M1Pwm;
+	_M1Dir = M1Dir;
+	_M2Pwm = M2Pwm;
+	_M2Dir = M2Dir;
 
   // I drive the pins low here over and over because the Pololu driver does
   //this to avoid ever driving the pins high by mistake. It seems there are
-  //certain boards that will do this when pinmode changes. I could
+  //certain boards that will do this when pinMode changes. I could
   //probably omit this, but better to be safe than sorry. See the Pololu
   //DRV8835 library (initPinsAndMaybeTimer() function) for more info.
-  digitalWrite(_M1PWM, LOW);
-  pinMode(_M1PWM, OUTPUT);
-  digitalWrite(_M1PWM, LOW);
-  digitalWrite(_M2PWM, LOW);
-  pinMode(_M2PWM, OUTPUT);
-  digitalWrite(_M2PWM, LOW);
-  digitalWrite(_M1DIR, LOW);
-  pinMode(_M1DIR, OUTPUT);
-  digitalWrite(_M1DIR, LOW);
-  digitalWrite(_M2DIR, LOW);
-  pinMode(_M2DIR, OUTPUT);
-  digitalWrite(_M2DIR, LOW);
+  digitalWrite(_M1Pwm, LOW);
+  pinMode(_M1Pwm, OUTPUT);
+  digitalWrite(_M1Pwm, LOW);
+  digitalWrite(_M2Pwm, LOW);
+  pinMode(_M2Pwm, OUTPUT);
+  digitalWrite(_M2Pwm, LOW);
+  digitalWrite(_M1Dir, LOW);
+  pinMode(_M1Dir, OUTPUT);
+  digitalWrite(_M1Dir, LOW);
+  digitalWrite(_M2Dir, LOW);
+  pinMode(_M2Dir, OUTPUT);
+  digitalWrite(_M2Dir, LOW);
   
   //Initialize the PWM channels
   //use the ESP32 function ledc to output PWM
-  ledcSetup(_M1CHN, PWM_FREQUENCY, PWM_PRECISION);
-  ledcAttachPin(_M1PWM, _M1CHN);
-  ledcSetup(_M2CHN, PWM_FREQUENCY, PWM_PRECISION);
-  ledcAttachPin(_M2PWM, _M2CHN);
+  ledcSetup(M1CHN, PWM_FREQUENCY, PWM_PRECISION);
+  ledcAttachPin(_M1Pwm, M1CHN);
+  ledcSetup(M2CHN, PWM_FREQUENCY, PWM_PRECISION);
+  ledcAttachPin(_M2Pwm, M2CHN);
 }
 
 //initialize the motor driver assuming the direction pins are connected to the MCP
 void BamBotMotorDriver::init(Adafruit_MCP23008 mcp,
-										byte M1PWM, 
-										byte M1DIR, 
-										byte M2PWM, 
-										byte M2DIR)
+										byte M1Pwm, 
+										byte M1Dir, 
+										byte M2Pwm, 
+										byte M2Dir)
 {
 	_USING_MCP = true;
 	_MCP = mcp;
 
 	//Record the motor pins in private variables
-	_M1PWM = M1PWM;
-	_M1DIR = M1DIR;
-	_M2PWM = M2PWM;
-	_M2DIR = M2DIR;
+	_M1Pwm = M1Pwm;
+	_M1Dir = M1Dir;
+	_M2Pwm = M2Pwm;
+	_M2Dir = M2Dir;
 
   // I drive the pins low here over and over because the Pololu driver does
   //this to avoid ever driving the pins high by mistake. It seems there are
   //certain boards that will do this when pinmode changes. I could
   //probably omit this, but better to be safe than sorry. See the Pololu
   //DRV8835 library (initPinsAndMaybeTimer() function) for more info.
-  digitalWrite(_M1PWM, LOW);
-  pinMode(_M1PWM, OUTPUT);
-  digitalWrite(_M1PWM, LOW);
-  digitalWrite(_M2PWM, LOW);
-  pinMode(_M2PWM, OUTPUT);
-  digitalWrite(_M2PWM, LOW);
+  digitalWrite(_M1Pwm, LOW);
+  pinMode(_M1Pwm, OUTPUT);
+  digitalWrite(_M1Pwm, LOW);
+  digitalWrite(_M2Pwm, LOW);
+  pinMode(_M2Pwm, OUTPUT);
+  digitalWrite(_M2Pwm, LOW);
   
   //initialize the direction pins on the MCP
-  _MCP.pinMode(_M1DIR, OUTPUT);
-  _MCP.pinMode(_M2DIR, OUTPUT);
+  _MCP.pinMode(_M1Dir, OUTPUT);
+  _MCP.pinMode(_M2Dir, OUTPUT);
   
   //Initialize the PWM channels
   //use the ESP32 function ledc to output PWM
-  ledcSetup(_M1CHN, PWM_FREQUENCY, PWM_PRECISION);
-  ledcAttachPin(_M1PWM, _M1CHN);
-  ledcSetup(_M2CHN, PWM_FREQUENCY, PWM_PRECISION);
-  ledcAttachPin(_M2PWM, _M2CHN);
+  ledcSetup(M1CHN, PWM_FREQUENCY, PWM_PRECISION);
+  ledcAttachPin(_M1Pwm, M1CHN);
+  ledcSetup(M2CHN, PWM_FREQUENCY, PWM_PRECISION);
+  ledcAttachPin(_M2Pwm, M2CHN);
 }
+
+/**********
+Motor Drive
+***********/
 
 // speed should be a number between -400 and 400
 void BamBotMotorDriver::setM1Speed(int speed)
@@ -119,13 +105,13 @@ void BamBotMotorDriver::setM1Speed(int speed)
 	}
 
 	//set the speed
-	_setPWM(_M1CHN, speed);
+	_setPWM(M1CHN, speed);
 
 	//set the direction
 	if (_USING_MCP) {
-		_MCP.digitalWrite(_M1DIR, reverse);
+		_MCP.digitalWrite(_M1Dir, reverse);
 	} else {
-		digitalWrite(_M1DIR, reverse);
+		digitalWrite(_M1Dir, reverse);
 	}
 }
 
@@ -142,13 +128,13 @@ void BamBotMotorDriver::setM2Speed(int speed)
 	}
 
 	//set the speed
-	_setPWM(_M2CHN, speed);
+	_setPWM(M2CHN, speed);
 	
 	//set the direction
 	if (_USING_MCP) {
-		_MCP.digitalWrite(_M2DIR, reverse);
+		_MCP.digitalWrite(_M2Dir, reverse);
 	} else {
-		digitalWrite(_M2DIR, reverse);
+		digitalWrite(_M2Dir, reverse);
 	}
 }
 
@@ -159,6 +145,7 @@ void BamBotMotorDriver::setSpeeds(int m1Speed, int m2Speed){
   setM2Speed(m2Speed);
 }
 
+//Set the reverse flag for the motors
 void BamBotMotorDriver::flipM1(bool flip)
 {
   _flipM1 = flip;
@@ -169,12 +156,149 @@ void BamBotMotorDriver::flipM2(bool flip)
   _flipM2 = flip;
 }
 
+//Private PWM function
+//Set the PWM output using the ledcWrite function
 void BamBotMotorDriver::_setPWM(uint8_t channel, uint32_t value) {
 	uint32_t duty = (8191/400)*value;
 	ledcWrite(channel, duty);
 }
 
+
+/****************
+Encoder Functions
+*****************/
+
+byte BamBotMotorDriver::_enc1PinA;
+byte BamBotMotorDriver::_enc1PinB;
+byte BamBotMotorDriver::_enc2PinA;
+byte BamBotMotorDriver::_enc2PinB;
+
+volatile int BamBotMotorDriver::_position1;
+volatile int BamBotMotorDriver::_position2;
+volatile byte BamBotMotorDriver::_state1;
+volatile byte BamBotMotorDriver::_state2;
+
+portMUX_TYPE BamBotMotorDriver::_mux = portMUX_INITIALIZER_UNLOCKED;
+
+//Initialize the encoders
 void BamBotMotorDriver::attachEncoders(byte pin1A, byte pin1B, byte pin2A, byte pin2B) {
-	encoder1.init(pin1A, pin1B);
-	encoder2.init(pin2A, pin2B);
+
+	//Record the encoder pins
+	_enc1PinA = pin1A;
+	_enc1PinB = pin1B;
+	_enc2PinA = pin2A;
+	_enc2PinB = pin2B;
+	
+	//Initialize the position and state variables
+	_position1 = 0;
+	_position2 = 0;
+	_state1 = 0;
+	_state2 = 0;
+	
+	//Initialize the encoder pins
+	pinMode(_enc1PinA, INPUT_PULLUP);
+	pinMode(_enc1PinB, INPUT_PULLUP);
+	pinMode(_enc2PinA, INPUT_PULLUP);
+	pinMode(_enc2PinB, INPUT_PULLUP);
+	
+	//Attach interrupts to moniter when the pins change state
+	attachInterrupt(digitalPinToInterrupt(_enc1PinA), _updatePosition, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(_enc1PinB), _updatePosition, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(_enc2PinA), _updatePosition, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(_enc2PinB), _updatePosition, CHANGE);
 }
+
+//Return the position for each motor
+int BamBotMotorDriver::motor1Position() {
+	return _position1;
+}
+
+int BamBotMotorDriver::motor2Position() {
+	return _position2;
+}
+
+//Reset the position to 0
+void BamBotMotorDriver::motor1ResetPosition() {
+	_position1 = 0;
+}
+
+void BamBotMotorDriver::motor2ResetPosition() {
+	_position2 = 0;
+}
+
+
+//This is where the position of the encoders are calculated based on the current and
+//previous states. There's some clever bit math here that I lifted from Paul
+//Stoffregen's Arduino Encoder library.
+//Check out Encoder.h from that library for more documentation on what this is doing
+//
+//In order to find out how much the encoder moved you need to compare the previous state
+//(pinA and pinB: 2 bits) + the current state (2 more bits). There are 16 possible cases
+//(4 bits total) and this code checks each one individually then records the current
+//state for the next call.
+void BamBotMotorDriver::_updatePosition() {
+	portENTER_CRITICAL_ISR(&_mux);
+	
+	int s;
+	
+	
+	//Update encoder 1
+	s = _state1 & 3;
+	
+	if (digitalRead(_enc1PinA)) s |= 4;
+	if (digitalRead(_enc1PinB)) s |= 8;
+	
+	switch (s) {
+		case 0: case 5: case 10: case 15:	//No movement
+			break;
+		case 1: case 7: case 8: case 14:		//one position forward
+			_position1++;
+			break;
+		case 2: case 4: case 11: case 13:	//one position backward
+			_position1--;
+			break;
+		case 3: case 12:							//two positions forward
+			_position1 += 2;
+			break;
+		default:										//two positions backward
+			_position1 -= 2;
+			break;
+	}
+	
+	_state1 = (s >> 2);
+	
+	
+	//Update encoder 2
+	s = _state2 & 3;
+
+	if (digitalRead(_enc2PinA)) s |= 4;
+	if (digitalRead(_enc2PinB)) s |= 8;
+	
+	switch (s) {
+		case 0: case 5: case 10: case 15:	//No movement
+			break;
+		case 1: case 7: case 8: case 14:		//one position forward
+			_position2++;
+			break;
+		case 2: case 4: case 11: case 13:	//one position backward
+			_position2--;
+			break;
+		case 3: case 12:							//two positions forward
+			_position2 += 2;
+			break;
+		default:										//two positions backward
+			_position2 -= 2;
+			break;
+	}
+
+	_state2 = (s >> 2);
+
+	
+	portEXIT_CRITICAL_ISR(&_mux);
+}
+
+
+
+
+
+
--- BamBotMotorDriver.h
+++ BamBotMotorDriver.h
@@ -3,43 +3,65 @@
 
 #include <Arduino.h>
 #include <Adafruit_MCP23008.h>
-#include "BamBotEncoder.h"
+
+//Default PWM channels, precision and frequency
+#define M1CHN 1
+#define M2CHN 2
+#define PWM_PRECISION 13
+#define PWM_FREQUENCY 5000
 
 class BamBotMotorDriver
 {
   public:
-    static void init(byte M1PWM = 3,
-    						byte M1DIR = 1, 
-    						byte M2PWM = 17, 
-    						byte M2DIR = 16);
-    static void init(Adafruit_MCP23008 mcp,
-    						byte M1PWM = 3,
-    						byte M1DIR = 7, 
-    						byte M2PWM = 1, 
-    						byte M2DIR = 6);
-    static void setM1Speed(int speed);
-    static void setM2Speed(int speed);
-    static void setSpeeds(int m1Speed, int m2Speed);
-    static void flipM1(boolean flip);
-    static void flipM2(boolean flip);
-    static void attachEncoders(byte pin1A, byte pin1B, byte pin2A, byte pin2B);
-    static BamBotEncoder encoder1;
-    static BamBotEncoder encoder2;
+    void init(byte M1Pwm = 3,
+    						byte M1Dir = 1, 
+    						byte M2Pwm = 17, 
+    						byte M2Dir = 16);
+    void init(Adafruit_MCP23008 mcp,
+    						byte M1Pwm = 3,
+    						byte M1Dir = 7, 
+    						byte M2Pwm = 1, 
+    						byte M2Dir = 6);
+    void setM1Speed(int speed);
+    void setM2Speed(int speed);
+    void setSpeeds(int m1Speed, int m2Speed);
+    void flipM1(boolean flip);
+    void flipM2(boolean flip);
+    
+    void attachEncoders(byte pin1A, byte pin1B, byte pin2A, byte pin2B);
+    int motor1Position();
+    int motor2Position();
+    void motor1ResetPosition();
+    void motor2ResetPosition();
+
+    static volatile int _position1;
+    static volatile int _position2;
+    static volatile byte _state1;
+    static volatile byte _state2;
+
   
   private:
-    static void _setPWM(uint8_t channel, uint32_t value);
-    static bool _USING_MCP;
-    static Adafruit_MCP23008 _MCP;
-    static byte _M1DIR;
-    static byte _M2DIR;
-    static byte _M1PWM;
-    static byte _M2PWM;
-    static byte _M1CHN;
-    static byte _M2CHN;
-    static int PWM_FREQUENCY;
-    static byte PWM_PRECISION;
-    static bool _flipM1;
-    static bool _flipM2;
+    void _setPWM(uint8_t channel, uint32_t value);
+    bool _USING_MCP;
+    Adafruit_MCP23008 _MCP;
+    byte _M1Dir;
+    byte _M2Dir;
+    byte _M1Pwm;
+    byte _M2Pwm;
+    bool _flipM1;
+    bool _flipM2;
+    
+    static byte _enc1PinA;
+    static byte _enc1PinB;
+    static byte _enc2PinA;
+    static byte _enc2PinB;
+    
+    //Anything referenced by an ISR must be declared volatile to prevent the compiler
+    //from applying optimizations that will keep the value from being properly updated
+    
+    static portMUX_TYPE _mux;
+    
+    static void IRAM_ATTR _updatePosition(int encoder);
 };
 
 #endif
